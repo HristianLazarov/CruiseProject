21c6da17b2e9e3f31516a7a5b637067f0ffb5eb0
Християн Лазаров <hl05089847@edu.mon.bg>
2024-12-02 17:07:04 +0200

    Fixed some frontend

--- Cruise_manager/Cruise_manager/obj/Debug/net8.0/Cruise_manager.AssemblyInfo.cs
+++ Cruise_manager/Cruise_manager/obj/Debug/net8.0/Cruise_manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cruise_manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2a7855507112bfe77dc3a6b2b7a53f35d887e51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cruise_manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cruise_manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
