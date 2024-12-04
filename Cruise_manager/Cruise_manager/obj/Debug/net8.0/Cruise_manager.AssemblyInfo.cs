91095e84ef7611cd49cecb5c6f4124f9ce25f639
Християн Лазаров <hl05089847@edu.mon.bg>
2024-12-04 14:56:37 +0200

    Repurposed the sign in page into a page for booking

--- Cruise_manager/Cruise_manager/obj/Debug/net8.0/Cruise_manager.AssemblyInfo.cs
+++ Cruise_manager/Cruise_manager/obj/Debug/net8.0/Cruise_manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cruise_manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f921d40906f01e9719dd1342adec53f50da5b769")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af6a9d5fbdebbae9df9430e7186e6d0d502b684b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cruise_manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cruise_manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
