09d445e8d803183a7a53600f2a01e49a1b914583
Християн Лазаров <hl05089847@edu.mon.bg>
2024-12-18 16:50:30 +0200

    Added new work on db

---
+++ Cruise_manager/Cruise_manager/Components/Data/Database.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+namespace Cruise_manager.Components.Data
+{
+    public class Database
+    {
+        static void DatabaseStart() 
+        {
+            string connectiontring = "Server=Cruise;Database=cruise;User ID=root;Password=toor;";
+            using (MySqlConnection connection = new MySqlConnection(connectiontring))
+            {
+            }
+        }
+    }
+}
--- Cruise_manager/Cruise_manager/obj/Debug/net8.0/Cruise_manager.AssemblyInfo.cs
+++ Cruise_manager/Cruise_manager/obj/Debug/net8.0/Cruise_manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cruise_manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7da976ad885c3290b340e4735c48309b4c4732e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+509c28aac254e4a8787a09136191dd038e6ae0e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cruise_manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cruise_manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
