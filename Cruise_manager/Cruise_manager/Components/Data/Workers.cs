0562aa8d59b81244d48a2e4c76d027ff09c0934e
Християн Лазаров <hl05089847@edu.mon.bg>
2025-01-06 17:04:27 +0200

    Started work on back end and added a page on back end

---
+++ Cruise_manager/Cruise_manager/Components/Data/Workers.cs
@@ -0,0 +1,28 @@
+﻿namespace Cruise_manager.Components.Data
+{
+    public class Workers
+    {
+        public Workers(string userName, string password, string email, string fname, string lname, string egn, string address, string phone, bool role)
+        {
+            UserName = userName;
+            Password = password;
+            Email = email;
+            Fname = fname;
+            Lname = lname;
+            EGN = egn;
+            Address = address;
+            Phone = phone;
+            Role = role;
+        }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string EGN { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public bool Role { get; set; }
+    }
+}
--- Cruise_manager/Cruise_manager/obj/Debug/net8.0/Cruise_manager.AssemblyInfo.cs
+++ Cruise_manager/Cruise_manager/obj/Debug/net8.0/Cruise_manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cruise_manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09d445e8d803183a7a53600f2a01e49a1b914583")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bcc7e7c6ee3f9f880f49e623040c218473dc77b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cruise_manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cruise_manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
